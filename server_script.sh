855700acbcd0aab43bb7e85aa02cd0be92911be0
Vlad Sanyuk <vlad.sanyuk@gmail.com>
2022-10-30 22:14:57 +0200

    fixed credentials

--- server_script.sh
+++ server_script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-artifactLink=$(curl -u server:12345678 -X GET 'http://0.0.0.0:8081/service/rest/v1/components?repository=maven-repo-2&sort=version' | jq '.items[].assets[].downloadUrl' --raw-output | grep ".jar$")
-wget --http-user server --http-password 12345678 $artifactLink
+if [[ $# -ne 2 ]]
+then
+	echo "Please, provide username and password as arguments"
+	echo "$0 <username> <password>"
+	exit 1
+fi
+username=$1
+echo $username
+password=$2
+echo $password
+artifactLink=$(curl -u $username:$password -X GET 'http://0.0.0.0:8081/service/rest/v1/components?repository=maven-repo-2&sort=version' | jq '.items[].assets[].downloadUrl' --raw-output | grep ".jar$")
+wget --http-user $username --http-password $password $artifactLink
 app=$(ls)
 java -jar $app
